244576e5e8c1c380bcd33fad5920e20d0197bcf7
Alvaroest <alvaro_rqsbo@hotmail.com>
2018-07-24 19:02:38 -0400

    excel apilados

--- datum/R/excel2.R
+++ datum/R/excel2.R
@@ -4,96 +4,90 @@
 ###############
 #library
 library(xlsx)
+library(tidyverse)
 ###############
 rm(list=ls())
 setwd("/Users/iMac6/Desktop/outside/2018/Banco/1udape/htms/CAP01") #capitulo 1
 ###############
-#xx<-dir()
-#for(zz in xx){
-  #print(zz)
-
-  
+#funciones para el excel
+multixls<-function(dd){
+  crna<-function(dd){
+  #columnas y filas con NA
+  aux<-dim(dd)
+  nar<-apply(apply(dd,1,is.na),2,sum)
+  nac<-apply(apply(dd,2,is.na),2,sum)
+  dd<-dd[,nac!=aux[1]]
+  dd<-dd[nar!=aux[2],]
+  #Casos con valores perdidos en la primera columna
+  dd<-dd[!is.na(dd[,1]),]
+  return(dd)
+  }
+  dd<-crna(dd)
+  #################
+  nar<-apply(apply(dd,1,is.na),2,sum)
+  nac<-apply(apply(dd,2,is.na),2,sum)
+  for(i in 1:dim(dd)[1]){
+    if(nar[i]==0){
+      iname<-i
+      break
+    }
+  }
+  #tablas multiples
+  aux2<-which(dd[,1]==dd[iname,1])
+  ini<-c(1,aux2[-1])
+  fin<-c(aux2[-1]-1,dim(dd)[1])
+  bd<-list()
+  for(i in 1:length(aux2)){
+    bd[[i]]<-dd[ini[i]:fin[i],]
+  }
+  bd<-lapply(bd, crna)
+  #ENCABEZADO
+  titulo<-function(dd){
+  nar<-apply(apply(dd,1,is.na),2,sum)
+  nac<-apply(apply(dd,2,is.na),2,sum)
+  for(i in 1:dim(dd)[1]){
+    if(nar[i]==0){
+      iname<-i
+      break
+    }
+  }
+  names(dd)<-dd[iname,]
+  dd<-dd[-iname,]
+  #detalle de la base
+  if(iname>1){dd<-dd[-(1:(iname-1)),]}
+  return(dd)
+  }
+  bd<-lapply(bd, titulo)
+  #fin de la base
+  notasfin<-function(dd){
+  nar<-apply(apply(dd,1,is.na),2,sum)
+  nac<-apply(apply(dd,2,is.na),2,sum)
+  for(i in dim(dd)[1]:1){
+    if(nar[i]==0){
+      iname<-i
+      break
+    }
+  }
+  dd<-dd[-((iname+1):dim(dd)[1]),]
+  return(dd)
+  }
+  bd<-lapply(bd, notasfin)
+  bdvf<-bd[[1]]
+  for(i in 2:length(bd)){
+    bdvf<-merge(bdvf,bd[[i]],all = T)
+  }
+  return(bdvf)
+}
 dd<-read.xlsx("c010101.xls",1,header=F,stringsAsFactors=FALSE)
-aux<-dim(dd)
-nar<-apply(apply(dd,1,is.na),2,sum)
-nac<-apply(apply(dd,2,is.na),2,sum)
-dd<-dd[,nac!=aux[1]]
-dd<-dd[nar!=aux[2],]
-nar<-apply(apply(dd,1,is.na),2,sum)
-nac<-apply(apply(dd,2,is.na),2,sum)
-#nombres
-for(i in 1:dim(dd)[1]){
-  if(nar[i]==0){
-    iname<-i
-    break
-  }
-}
-names(dd)<-dd[iname,]
-dd<-dd[-4,]
-#detalle de la base
-det<-""
-for(i in 1:dim(dd)[2]){
-  for(j in 1:(iname-1)){
-    if(!is.na(dd[j,i])){
-      det<-paste(det,dd[j,i])
-    }
-  }
-}
-dd<-dd[-(1:(iname-1)),]
-#fin de la base
-nar<-apply(apply(dd,1,is.na),2,sum)
-nac<-apply(apply(dd,2,is.na),2,sum)
-for(i in dim(dd)[1]:1){
-  if(nar[i]==0){
-    iname<-i
-    break
-  }
-}
-#Pie de notas
-pie<-""
-for(i in 1:dim(dd)[2]){
-  for(j in dim(dd)[1]:(iname+1)){
-    if(!is.na(dd[j,i])){
-      pie<-paste(pie,dd[j,i])
-    }
-  }
-}
-dd<-dd[-((iname+1):dim(dd)[1]),]
-
-#valores perdidos en la primera columna
-dd<-dd[!is.na(dd[,1]),]
-#bases apiladas
-fin<-dim(dd)
-aux<-dd[fin[1],1]
-auxvec<-NULL
-for(i in 1:fin[1]){
-  print(i)
-  if(dd[i,1]==aux){
-    auxvec<-c(auxvec,i)
-  }
-}
-bd<-list()
-zz<-1
-for(i in length(auxvec)){
-  bd[[i]]<-dd[1+auxvec[i]*(zz-1):auxvec[i],]
-  if(i>2){zz<-2}
-  zz<-zz+1
-}
-head(dd,18)
-str(bd)
-i<-1;zz<-1
-head(dd[1+auxvec[i]*(zz-1):auxvec[i],])
+bd<-multixls(dd)
 #tipo de variables a numericas
-for(i in 2:dim(dd)[2]){
-  if(!is.numeric(dd[,i])){
-    dd[,i]<-as.numeric(dd[,i])
+for(i in 2:dim(bd)[2]){
+  print(i)
+  if(!is.numeric(bd[,i])){
+    bd[,i]<-as.numeric(bd[,i])
   }
 }
 #csv
 nn<-strsplit(zz,".xls")[[1]]
 nn<-paste0(nn,".csv")
 write.table(dd,nn,sep = ";")
-}
-
-
-
