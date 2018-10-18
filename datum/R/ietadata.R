f81ad827155cd2f45066b05a8bdad215dd6b9a72
Alvaroest <alvaro_rqsbo@hotmail.com>
2018-10-18 08:23:52 -0400

    ietadata r

---
+++ datum/R/ietadata.R
@@ -0,0 +1,9 @@
+##############
+#ietadata
+#autor: Alvaro Chirino
+##############
+ietadata<-function(key="eh17v"){
+  url<-paste0("https://ieta-umsa.github.io/datum/data/",key,".csv")
+  data<-read.csv2(url)
+  return(data)
+}
