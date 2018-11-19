55ab570dfec537b1d1e5a46e167fc21a6198049b
Alvaro Limber Chirino Gutierrez <alvaro.rqsbo@gmail.com>
2018-11-19 12:51:45 -0400

    a

--- datum/R/ietadata.R
+++ datum/R/ietadata.R
@@ -4,6 +4,6 @@
 ##############
 ietadata<-function(key="eh17v"){
   url<-paste0("https://ieta-umsa.github.io/datum/data/",key,".csv")
-  data<-read.csv2(url)
+  data<-read.csv2(url,stringsAsFactors=F)
   return(data)
 }
